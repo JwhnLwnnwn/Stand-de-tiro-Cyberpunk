dd02b55b09e1963a49a6d53d6e122f119209a455
Ingryd <ingrydcduarte@gmail.com>
2026-03-26 18:06:32 -0300

    devlog: tutorial animation blink and import assets

--- StandCypherpunkDemo/scenes/tutorial_scene.gd
+++ StandCypherpunkDemo/scenes/tutorial_scene.gd
@@ -1,10 +1,18 @@
 extends CanvasLayer
 
-@onready var label = $back_btn/back_label
+@onready var control = $Control
+@onready var bg = $Control/Background
+@onready var icon = $Control/back_btn/back_label
 @onready var click_sfx = $SFX
+
 var normal_tex = preload("res://UI/button/left_seta.png")
 var pressed_tex = preload("res://UI/button/pressed_left_btn.png")
-signal closed;
+
+var tut_gray_tex = preload("res://UI/tutorial/tutorial_gray.png")
+var tut_neon_tex = preload("res://UI/tutorial/tutorial_neon.png")
+var tut_final_tex = preload("res://UI/tutorial/tela_tutorial.png")
+
+signal closed
 
 enum Mode {
 	MENU,
@@ -16,59 +24,57 @@ var current_mode = Mode.MENU
 func _ready() -> void:
 	process_mode = Node.PROCESS_MODE_ALWAYS
 	visible = false
-	print(label)
-	print(label.texture)
 
 func show_tutorial(mode) -> void:
 	current_mode = mode
 	visible = true
 	
-	for child in get_children():
-		if child is CanvasItem:
+	for child in control.get_children():
+		if child is CanvasItem and child != bg:
 			child.modulate.a = 0.0
 	
 	await get_tree().process_frame
 	
-	# O efeito de piscar apenas no sprite2D -> TODO: Alterar isso para funcionar nos containers (esperando os sprites)
-	for child in get_children():
-		if child is Sprite2D:
-			await _blink_node(child)
+	await _blink_background()
 	
-	for child in get_children():
-		if child is CanvasItem and not (child is Sprite2D):
+	for child in control.get_children():
+		if child is CanvasItem and child != bg:
 			_fade_in(child)
 
-func _blink_node(node: CanvasItem) -> void:
-	var tween = create_tween()
-	tween.set_loops(3)
-	tween.tween_property(node, "modulate:a", 1.0, 0.15)
-	tween.tween_property(node, "modulate:a", 0.0, 0.15)
-	await tween.finished
-	node.modulate.a = 1.0
+func _blink_background() -> void:
+	var times = 4
+	
+	for i in range(times):
+		bg.texture = tut_gray_tex
+		await get_tree().create_timer(0.08).timeout
+		
+		bg.texture = tut_neon_tex
+		await get_tree().create_timer(0.08).timeout
+	
+	bg.texture = tut_final_tex
 
 func _fade_in(node: CanvasItem) -> void:
 	var tween = create_tween()
-	tween.tween_property(node, "modulate:a", 1.0, 0.5)
-
+	tween.tween_property(node, "modulate:a", 1.0, 0.3)
 
 func _on_back_btn_pressed() -> void:
-	label.texture = pressed_tex
-	label.scale = Vector2(0.95, 0.95)
-
+	icon.pivot_offset = icon.size / 2
+	
+	icon.texture = pressed_tex
+	icon.scale = Vector2(0.95, 0.95)
+	
 	click_sfx.play()
 
-func _on_back_btn_button_up():
+func _on_back_btn_button_up() -> void:
 	await get_tree().create_timer(0.1).timeout
-	label.texture = normal_tex
-	label.texture = normal_tex
+	
+	icon.texture = normal_tex
+	
 	var tween = create_tween()
-	tween.tween_property(label, "scale", Vector2(1,1), 0.1)
+	tween.tween_property(icon, "scale", Vector2(1, 1), 0.1)
 
-# Essa tela de tutorial aparece tanto em MENU como PAUSE
-	match current_mode:
-		Mode.MENU:
-			visible = false
+	_close()
 
-		Mode.PAUSE:
-			visible = false
+func _close() -> void:
+	visible = false
 	emit_signal("closed")
